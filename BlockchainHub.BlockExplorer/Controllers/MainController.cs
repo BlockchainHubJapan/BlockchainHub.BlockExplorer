51e8a0fa094f59bdded30b5ffef2f554f67e708c
NicolasDorier <nicolas.dorier@gmail.com>
2016-10-13 17:05:08 +0900

    Can see blocks on main page

--- BlockchainHub.BlockExplorer/Controllers/MainController.cs
+++ BlockchainHub.BlockExplorer/Controllers/MainController.cs
@@ -1,37 +1,90 @@
-﻿using System;
+﻿using BlockchainHub.BlockExplorer.Models;
+using NBitcoin;
+using QBitNinja.Client;
+using QBitNinja.Client.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace BlockchainHub.BlockExplorer.Controllers
 {
-    public class MainController : Controller
-    {
-        [Route("")]
-        public ActionResult Index(string search = null)
-        {
-            if(string.IsNullOrWhiteSpace(search))
-                return View();
-            return View();
-        }
+	public class MainController : Controller
+	{
+		QBitNinjaClient QBit = new QBitNinjaClient(Network.Main);
 
-        [Route("addresses")]
-        public ActionResult Address()
-        {
-            return View();
-        }
+		[Route("")]
+		public async Task<ActionResult> Index(string search = null, int count = 5)
+		{
+			count = Math.Max(0, count);
+			count = Math.Min(50, count);
+			if(!string.IsNullOrWhiteSpace(search))
+				return View();
 
-        [Route("blocks")]
-        public ActionResult Block()
-        {
-            return View();
-        }
+			var responses =
+				await Task.WhenAll(Enumerable
+				.Range(0, count)
+				.Select(i => QBit.GetBlock(new BlockFeature(SpecialFeature.Last) { Offset = -i }, true, true))
+				.ToArray());
 
-        [Route("transactions")]
-        public ActionResult Transaction()
-        {
-            return View();
-        }
-    }
+			var model = new MainModel();
+			model.NextCount = count + 5;
+			foreach(var response in responses.Where(r => r.ExtendedInformation != null && r.AdditionalInformation != null))
+			{
+				var blockModel = new MainBlockModel();
+				blockModel.Hash = response.AdditionalInformation.BlockId;
+				blockModel.Height = response.AdditionalInformation.Height;
+				blockModel.Size = ToKB(response.ExtendedInformation.Size);
+				blockModel.Time = ToRelative(response.AdditionalInformation.BlockTime);
+				blockModel.TransactionsCount = response.ExtendedInformation.TransactionCount;
+				blockModel.Fees = (response.ExtendedInformation.BlockReward - response.ExtendedInformation.BlockSubsidy).ToDecimal(MoneyUnit.BTC).ToString();
+				model.Blocks.Add(blockModel);
+			}
+			return View(model);
+		}
+
+		private string ToRelative(DateTimeOffset time)
+		{
+			var ago = DateTimeOffset.UtcNow - time;
+			if(ago.TotalMinutes < 1.0)
+			{
+				return (int)(ago.TotalSeconds) + " seconds ago";
+			}
+			else if(ago.TotalHours < 1.0)
+			{
+				return (int)(ago.TotalMinutes) + " minutes ago";
+			}
+			else
+			{
+				return (int)(ago.TotalHours) + " h, " + (ago.Minutes) + " min ago";
+			}
+		}
+
+		private string ToKB(int size)
+		{
+			decimal kb = (decimal)size / 1000.0m;
+			kb = decimal.Round(kb, 3);
+			return kb + " kB";
+		}
+
+		[Route("addresses")]
+		public ActionResult Address()
+		{
+			return View();
+		}
+
+		[Route("blocks/{blockId}", Name = "Block")]
+		public ActionResult Block(uint256 blockId)
+		{
+			return View();
+		}
+
+		[Route("transactions")]
+		public ActionResult Transaction()
+		{
+			return View();
+		}
+	}
 }
---
+++ BlockchainHub.BlockExplorer/Models/MainModel.cs
@@ -0,0 +1,60 @@
+﻿using NBitcoin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlockchainHub.BlockExplorer.Models
+{
+	public class MainModel
+	{
+		public MainModel()
+		{
+			Blocks = new List<MainBlockModel>();
+		}
+		public List<MainBlockModel> Blocks
+		{
+			get;
+			set;
+		}
+		public int NextCount
+		{
+			get;
+			set;
+		}
+	}
+
+	public class MainBlockModel
+	{
+		public uint256 Hash
+		{
+			get;
+			set;
+		}
+		public int Height
+		{
+			get;
+			set;
+		}
+		public string Time
+		{
+			get;
+			set;
+		}
+		public int TransactionsCount
+		{
+			get;
+			set;
+		}
+		public string Fees
+		{
+			get;
+			set;
+		}
+		public string Size
+		{
+			get;
+			set;
+		}
+	}
+}
