8a8dc15b1c4c55ac384f84ab8920874cf5bf914a
NicolasDorier <nicolas.dorier@gmail.com>
2016-10-15 16:43:41 +0900

    fix tx link

--- BlockchainHub.BlockExplorer/Scripts/main.js
+++ BlockchainHub.BlockExplorer/Scripts/main.js
@@ -1,29 +1,33 @@
-$(function(){
-	function mobileVersion()
-	{
-		var ret = true;
-		if ( $("#mobile-version").css("display") == "none" )
-			ret = false;
-		return ret;
-	}
-	
-	$(".search-quit").click(function(){
-		$(".header-search-view").hide();
-		$(".header-default-view").show();
-	});
-	
-	$(".search-show").click(function(){
-		if ( !mobileVersion() )
-		{
-			$(".header-default-view").hide();
-			$(".header-search-view").show();
-		}
-	});
-	
-	$(".accordion-custom .panel-heading").click(function(){
-		var parent = $(this).parent();
-		$(".custom-collapse-active").not(parent).removeClass("custom-collapse-active");
-		$(this).parent().toggleClass("custom-collapse-active");
-		console.log("b");
-	});
+$(function () {
+    function mobileVersion() {
+        var ret = true;
+        if ($("#mobile-version").css("display") == "none")
+            ret = false;
+        return ret;
+    }
+
+    $(".search-quit").click(function () {
+        $(".header-search-view").hide();
+        $(".header-default-view").show();
+    });
+
+    $(".search-show").click(function () {
+        if (!mobileVersion()) {
+            $(".header-default-view").hide();
+            $(".header-search-view").show();
+        }
+    });
+
+    $(".accordion-custom .panel-heading").click(function () {
+        var parent = $(this).parent();
+        $(".custom-collapse-active").not(parent).removeClass("custom-collapse-active");
+        $(this).parent().toggleClass("custom-collapse-active");
+    });
+
+
+    $(".panel-heading a").click(function () {
+        event.stopPropagation();
+        return true;
+    });
+
 });
